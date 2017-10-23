1f6ae62be1eaa55491836b29cd490f46064725e6
yanqi.hou <yanqi.hou@anansimobile.org>
2017-10-23 22:12:41 +0800

    add test, confgured launch.json;

--- app.test.ts
+++ app.test.ts
@@ -1,8 +1,44 @@
 import * as chai from 'chai';
 import * as request from 'supertest';
 import * as express from 'express';
+import * as log4js from 'log4js';
 import { server } from './app';
 
 const app = server.app;
 const expect = chai.expect;
-request(app).post('/creategame').expect(200)
+const logger = log4js.getLogger("test");
+
+describe("创建比赛", () => {
+  it("创建比赛成功", (done) => {
+    request(app)
+      .post('/creategame')
+      .send({
+        formData: {
+          gameName: "2135",
+          gameDate: "2017-01-11",
+          gameEndTime: "23:59",
+          gameTime: "00:18",
+          refereeNumber: "3",
+          openid: "65489784941",
+        }
+      })
+      .expect(200, (err, res) => {
+        expect(err).to.be.equal(null);
+        done();
+      })
+  })
+})
+
+describe("获取openid", () => {
+  it("获取到openid", done => {
+    request(app)
+      .post('openid')
+      .send({
+        code: "6651818s1ad8f1",
+      })
+      .expect(200, (err, res) => {
+        logger.debug('openid是：', res);
+        done();
+      });
+  })
+})
--- bin/app.test.js
+++ bin/app.test.js
@@ -1,13 +1,43 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var chai = require("chai");
+var request = require("supertest");
+var log4js = require("log4js");
 var app_1 = require("./app");
 var app = app_1.server.app;
 var expect = chai.expect;
-// request(app).post('/creategame').expect(200)
-describe("创建比赛数据", function () {
-    it("创建成功", function () {
-        expect(1).to.be.eql(2);
+var logger = log4js.getLogger("test");
+describe("创建比赛", function () {
+    it("创建比赛成功", function (done) {
+        request(app)
+            .post('/creategame')
+            .send({
+            formData: {
+                gameName: "2135",
+                gameDate: "2017-01-11",
+                gameEndTime: "23:59",
+                gameTime: "00:18",
+                refereeNumber: "3",
+                openid: "65489784941",
+            }
+        })
+            .expect(200, function (err, res) {
+            expect(err).to.be.equal(null);
+            done();
+        });
+    });
+});
+describe("获取openid", function () {
+    it("获取到openid", function (done) {
+        request(app)
+            .post('openid')
+            .send({
+            code: "6651818s1ad8f1",
+        })
+            .expect(200, function (err, res) {
+            logger.debug('openid是：', res);
+            done();
+        });
     });
 });
 //# sourceMappingURL=app.test.js.map
